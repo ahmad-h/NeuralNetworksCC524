03005a40f714e737767589cc1757a2718b083bde
The Night Phoenix <abdallah.hodieb@gmail.com>
2014-02-23 06:11:17 +0200

    week 1 updated

---
+++ Week01/code.m
@@ -0,0 +1,32 @@
+% this is a comment 
+
+% Simple arithmatic
+1 + 5
+2 - 9
+5 * 3.14159265369
+5/2
+1/0 % thats infinaty :)
+
+
+% Vectors and Matrices
+v1 = [ 1 2 3 4 5 6 7] % row
+v2 = [ 1 ;2 ; 3 ; 4 ; 5 ;6 ;7] % colomn
+v1' %transpose
+v2' %transpose
+
+
+
+m1 = [1 2 3 ;4 5 6;7 8 9]
+m1'
+
+
+v1 = 1:5 % numbers from 1 to 5
+
+v2 = 0:2:10 %even numbers from 0 to 10
+
+
+v1 * 5 % each item in v1 is multiplied by 5
+
+
+
+
